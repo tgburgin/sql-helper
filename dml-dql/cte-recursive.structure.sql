427df0b46df1362f33d4d24505db37212c2ef53a
Tom Burgin <tburgin@keytravel.com>
2020-06-10 10:38:02 +0100

    Add recursive CTE

---
+++ dml-dql/cte-recursive.structure.sql
@@ -0,0 +1,12 @@
+WITH expression_name (column_list)
+AS
+(
+    -- Anchor member
+    initial_query  
+    UNION ALL
+    -- Recursive member that references expression_name.
+    recursive_query  
+)
+-- references expression name
+SELECT *
+FROM   expression_name
